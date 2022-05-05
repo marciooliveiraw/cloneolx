7815a8bdb3521f1a80442ee6565aa53b79534414
Márcio Oliveira <marciooliveiraw@gmail.com>
2022-05-05 09:31:27 -0300

    Projeto olx em andamento - atualmente em módulo 11 aula 29

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React from "react";
+import { connect } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import { Template } from "./components/MainComponents";
+import Header from "./components/partials/Header";
+import Footer from './components/partials/Footer';
+
+import { Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import AdPage from './pages/AdPage';
+import NotFound from './pages/NotFound';
+
+const Page = (props) => {
+  return(
+    <BrowserRouter>
+      <Template>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/ad/:id" element={<AdPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </Template>
+    </BrowserRouter>
+
+    
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return{
+    user:state.user
+  };
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return{
+
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Page);
